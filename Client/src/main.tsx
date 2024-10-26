ed74e764244982799d0615eb4ec0b55bd3b3e9bd
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-26 11:37:59 -0400

    Update main.tsx

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,3 +1,5 @@
+import {NextUIProvider} from '@nextui-org/react'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
@@ -6,6 +8,8 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
   </StrictMode>,
 )
