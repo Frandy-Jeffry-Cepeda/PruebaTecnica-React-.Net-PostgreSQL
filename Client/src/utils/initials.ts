d64e6bf3364fde82bb87c1decb4e50b8051fc41a
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-26 11:37:48 -0400

    Create initials.ts

---
+++ Client/src/utils/initials.ts
@@ -0,0 +1,12 @@
+export function getInitials(name: string) {
+
+    const split = name.split(" ");
+    
+    if (split.length === 4) {
+      return (split[0][0] + split[2][0]).toUpperCase();
+    } else if (split.length === 3) {
+      return (split[0][0] + split[1][0]).toUpperCase();
+    }
+    
+    return split.map(word => word[0]).join("").toUpperCase();
+}
