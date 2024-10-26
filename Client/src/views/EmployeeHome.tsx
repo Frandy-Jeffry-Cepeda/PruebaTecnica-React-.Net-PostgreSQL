7c2232d28b2f9350b681a71ac617772384b2133b
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-26 09:11:18 -0400

    Update EmployeeHome.tsx

--- Client/src/views/EmployeeHome.tsx
+++ Client/src/views/EmployeeHome.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+
 
 export default function EmployeeHome() {
   return (
-    <div>EmployeeHome</div>
+    <div className="mt-10 mx-auto max-w-6xl p-6 sm:p-10 bg-white shadow-lg">
+      <div className="flex justify-between">
+        <h2>Bienvenido, Frandy Jeffry Cepeda </h2>
+      </div>
+    </div>
   )
 }
