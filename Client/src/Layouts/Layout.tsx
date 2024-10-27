f7846c29497dcfe091bb250e23808b9c9af69264
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-27 06:02:39 -0400

    Update Layout.tsx

--- Client/src/Layouts/Layout.tsx
+++ Client/src/Layouts/Layout.tsx
@@ -1,15 +1,28 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Notification from '../Components/Notification'
+import { LogOutIcon } from 'lucide-react'
+import { deleteToken } from '../utils/auth'
 
 export default function Layout() {
+
+  const navigate = useNavigate()
+
+  function handleLogout() {
+    deleteToken()
+    navigate('/')
+  }
+
   return (
     <>
 
       <header className="bg-slate-800">
-        <div className="max-w-6xl mx-auto py-10 flex justify-center items-center">
+        <div className="max-w-6xl mx-auto py-10 flex justify-between items-center">
           <h1 className="text-4xl font-extrabold text-white">
             Prueba Técnica
           </h1>
+          <button onClick={handleLogout}>
+            <LogOutIcon className='text-danger'></LogOutIcon>
+          </button>
         </div>
       </header>
 
